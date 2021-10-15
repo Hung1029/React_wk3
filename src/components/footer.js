aa3c8404b13121faa4774b8d545da94b855b2a35
Hung1029 <39110096+Hung1029@users.noreply.github.com>
2021-10-15 17:36:01 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div>
            <div className="row0 footer">
-        <div className="col-md-2">
+        <div className="col-md-1">
           <a href="#"><i className="fab fa-facebook-square fa-1_5x i_color"></i></a>
           <a href="#"><i className="far fa-envelope fa-1_5x i_color"></i></a>
           <a href="#"><i className="fab fa-instagram fa-1_5x i_color"></i></a>
